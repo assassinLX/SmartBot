5fe1fa854c79794430931bbc7147705b418e0393
skyRRR <2451978254@qq.com>
2018-05-06 17:36:37 +0800

    完善了3-2场景，帮助界面修改完，初步添加跳转

---
+++ SmartBot/SmartBot/SmartBot_18_5_1/Assets/Script/RQ_chooseLever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RQ_chooseLever : MonoBehaviour {
+
+    public Button start;
+
+    public void Awake()
+    {
+        //start = GameObject.Find("");
+        start.onClick.AddListener(() => gotoOne());
+    }
+
+    public void gotoOne()
+    {
+        SceneManager.LoadScene("no1-1");
+    }
+
+
+}
